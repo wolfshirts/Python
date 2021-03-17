47f721b6e53e9739010113bc36dd2778bd7a68e4
Ross Russell <ross.russell@protonmail.com>
2021-03-17 16:37:37 -0700

    writing queries.

--- QA/models/queries.js
+++ QA/models/queries.js
@@ -1,5 +1,28 @@
 const pool = require("./pg.js");
 
+const getQuestions = (productId, queryObj, cb) => {};
+const getAnswers = (questionId, queryObj, cb) => {};
+
+const postNewQuestion = (questionId, queryObj, cb) => {};
+const postNewAnswer = (questionId, queryObj, cb) => {
+  // CURRENT_DATE gives us the format we need.
+  const { question_id, body, name, email, photos } = queryObj;
+  const answerInsert = [question_id, body, name, email];
+  pool.query(
+    "INSERT INTO answers (question_id, body, date_written, answerer_name, answerer_email) VALUES($1, $2, CURRENT_DATE, $3, $4",
+    answerInsert,
+    (err, result) => {
+      if (err) {
+        cb(err, null);
+      } else if (photos !== null) {
+        // If there are photos we're not done. We need to insert them as well.
+      } else {
+        cb(null, result);
+      }
+    }
+  );
+};
+
 const markAnswerAsHelpful = (answerId, cb) => {
   pool.query(
     "UPDATE answers SET helpful=(helpful+1) WHERE id=$1",
@@ -61,4 +84,8 @@ module.exports = {
   reportQuestion,
   markAnswerAsHelpful,
   reportAnswer,
+  postNewAnswer,
+  postNewQuestion,
+  getAnswers,
+  getQuestions,
 };
