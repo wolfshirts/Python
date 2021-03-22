4052dc8375252feb71dd69c563c5d77969057cab
Ross Russell <ross.russell@protonmail.com>
2021-03-22 15:06:14 -0700

    looking ready for deployment. 6ms localhost response

--- QA/etlTool/index.js
+++ QA/etlTool/index.js
@@ -82,6 +82,7 @@ const addIndexes = () => {
   pool.query("CREATE INDEX answerid ON photos(answer_id)");
   pool.query("CREATE INDEX questionid ON answers(question_id)");
   pool.query("CREATE INDEX prodid ON questions(product_id)");
+  pool.query("CREATE INDEX reported ON questions(reported)");
 };
 
 // const fileObject = {
